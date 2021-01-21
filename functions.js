f3a8ab0c557b2d11747d5a26167a366869d74031
Adelinanica <adelinanica29@yahoo.com>
2021-01-21 19:46:08 +0200

    fix hideAllPages function

--- functions.js
+++ functions.js
@@ -8,14 +8,14 @@ function show(id) {
 }
 
 function hideAllPages(){
-    var pagesIds = ["home", "skills", "languages", "projects"];
-    console.debug(pagesIds);
-
+    var pages = document.querySelectorAll("#main .page");
+    
     //initial ;         condition     ; post execution
-    for( var i = 0; i < pagesIds.length; i++) {
-        console.info("i= ", i);
-        hide(pagesIds[i]);
-    }
+    for( var i = 0; i < pages.length; i++) {
+        var page = pages[i];
+        hide(page.id);  
+        // hide(pages[i].id)     
+        }
 }
 
 function showHomePage() {
