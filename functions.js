fa3dc49f6ff9fbada2ae2f0f98c6dc2acd12aad4
Adelinanica <adelinanica29@yahoo.com>
2021-01-28 21:01:26 +0200

    use string template inside map

--- functions.js
+++ functions.js
@@ -41,7 +41,8 @@ var allSkills = [
 //TO DO class="favourite-skill"
 
 var allSkillsHtml = allSkills.map(function (skill){
-  return `<li>`+ skill +`</li>`;
+  //return `<li>`+ skill +`</li>`;
+    return  `<li>${skill}</li>`;
 });
 
 var skillsEl = document.querySelector("#skills ul");
